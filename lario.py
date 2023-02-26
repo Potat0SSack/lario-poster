6b9441c9502391972896c79c17b69958b39564bb
PotatOS <kartofel_ryabov@vk.com>
2023-02-26 13:27:44 +0300

    Add files via upload

---
+++ lario.py
@@ -0,0 +1,31 @@
+# bot.py
+import os
+import random
+
+import discord
+from discord.ext import tasks
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+    channel = await client.fetch_channel(1079096623763820664) 
+    @tasks.loop(minutes=5)
+
+    async def sendmessage():
+        await channel.send("Wheezy outta here")
+
+        post = random.choice(os.listdir("./posts/"))
+        await channel.send(file=discord.File('./posts/' + post))
+        
+    await sendmessage.start()
+
+
+client.run(TOKEN)
